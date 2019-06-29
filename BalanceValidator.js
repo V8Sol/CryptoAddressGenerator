5631f7e76753c62aa537a7a3373b96fd596ec9a1
Vimal Bera <vimalbera92@gmail.com>
2019-06-29 13:21:46 +0530

    Update balance check logic

--- BalanceValidator.js
+++ BalanceValidator.js
@@ -3,59 +3,63 @@ const mysql = require("mysql")
 const util = require('util');
 
 const con = mysql.createConnection({
-    host: "crypto.ca2iecww9r6d.ap-south-1.rds.amazonaws.com",
-    user: "trading_bot",
-    password: "Rspl123#",
-    database: "crypto_world"
+  host: "crypto.ca2iecww9r6d.ap-south-1.rds.amazonaws.com",
+  user: "trading_bot",
+  password: "Rspl123#",
+  database: "crypto_world"
 });
 
 const query = util.promisify(con.query).bind(con);
 
 const getBalance = async (address, coin) => {
-    try {
-        let url = ''
-        if (coin == 'BTC') {
-            url = `https://blockchain.info/q/addressbalance/${address}`
-        } else if (coin == 'ETH') {
-            url = `https://ethplorer.io/service/service.php?data=${address}`
-        } else if (coin == 'XRP') {
-            url = `https://data.ripple.com/v2/accounts/${address}/balances`
-        }
-        const response = await axios.get(url);
-	var balance = parseFloat(response.data)
-	if(coin == 'ETH') {
-		balance = parseFloat(response.data.balance)
-	}
-        return Promise.resolve(balance)
-    } catch (error) {
-        return Promise.resolve(0);
+  try {
+    let url = ''
+    if (coin == 'BTC') {
+      url = `https://api.blockcypher.com/v1/btc/main/addrs/${address}/balance`
+    } else if (coin == 'ETH') {
+      url = `https://ethplorer.io/service/service.php?data=${address}`
+    } else if (coin == 'XRP') {
+      url = `https://data.ripple.com/v2/accounts/${address}/balances`
     }
+    const response = await axios.get(url);
+    if (coin == 'BTC') {
+      var balance = parseFloat(response.data.balance + response.data.total_received)
+    } else if (coin == 'ETH') {
+      var balance = parseFloat(response.data.balance) + parseFloat(response.data.transfers.length)
+    } else {
+      var balance = parseFloat(response.data)
+    }
+    return Promise.resolve(balance)
+  } catch (error) {
+    return Promise.resolve(0);
+  }
 };
 
 const run = async () => {
-    let index = 0
-    try {
-        while (true) {
-            let sql = `SELECT * FROM address where balance is null limit 1`;
-            let results = await query(sql)
-            const balance = await getBalance(results[0].address, results[0].coin)
-	    console.log(balance)
-            if (balance > 0) {
-                axios.post('https://hooks.slack.com/services/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', {
-                    "text": `Found ${results[0].coin} address ${results[0].address} with balance ${balance}`
-                }).then(function (response) {
-                    console.log(response);
-                }).catch(function (error) {
-                    console.log(error);
-                })
-            }
-            let updateSql = `UPDATE address set balance = ${balance} where id = ${results[0].id}`
-            query(updateSql)
-            index++
-        }
-    } catch(err) {
-        console.log(err)
-        index--;
+  let index_sql = `SELECT * FROM address where balance is null limit 1`;
+  let first_record = await query(index_sql)
+  let index = first_record[0].id
+  try {
+    while (true) {
+      let sql = `SELECT * FROM address where id = ${index}`;
+      let results = await query(sql)
+      const balance = await getBalance(results[0].address, results[0].coin)
+      if (balance > 0) {
+        axios.post('https://hooks.slack.com/services/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', {
+          "text": `Found ${results[0].coin} address ${results[0].address} with balance ${balance}`
+        }).then(function (response) {
+          console.log(response);
+        }).catch(function (error) {
+          console.log(error);
+        })
+      }
+      let updateSql = `UPDATE address set balance = ${balance} where id = ${results[0].id}`
+      query(updateSql)
+      index++
     }
+  } catch (err) {
+    console.log(err)
+    index--;
+  }
 }
 run()
