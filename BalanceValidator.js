13f3beec1559d6273dafed42807f132ef3627394
Ubuntu <ubuntu@ip-172-31-20-11.ap-south-1.compute.internal>
2019-07-13 10:29:27 +0000

    Update

--- BalanceValidator.js
+++ BalanceValidator.js
@@ -39,6 +39,7 @@ const run = async () => {
   let index_sql = `SELECT * FROM address where balance is null limit 1`;
   let first_record = await query(index_sql)
   let index = first_record[0].id
+	console.log(index);
   try {
     axios.post('https://hooks.slack.com/services/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', {
       "text": "Address scanning started..."
