055b82bd2b476774312d23ebbfa75541cabd44d1
Vimal Bera <vimal.bera@qomplx.com>
2019-11-07 11:51:35 +0530

    Update eth balance checker

--- src/eth/ether.js
+++ src/eth/ether.js
@@ -2,15 +2,10 @@ const web3 = require("web3");
 const axios = require("axios");
 
 exports.getBalance = async address => {
-  const body = {
-    id: 1,
-    jsonrpc: "2.0",
-    method: "eth_getBalance",
-    params: [`${address}`, "latest"]
-  }
-  const url = `https://mainnet.infura.io/v3/f221ad528a584387950f61e9d0baebbb`
-  const response = await axios.post(url, body);
-  return Promise.resolve(response.data.result != '0x0' ? 1 : 0)
+  //api key: NVDGQFJ7FXPYK1MQ6D586F6FZ6JRFM2ZGI
+  const url = `https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=YourApiKeyToken`
+  const response = await axios.get(url);
+  return Promise.resolve(response.data.result)
 }
 
 exports.getKeyPair = () => {
