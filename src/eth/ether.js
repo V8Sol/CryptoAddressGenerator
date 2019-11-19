59ed9d699890c9bf7cf6e098a040d8a1e5a1d34d
Vimal Bera <vimalbera92@gmail.com>
2019-11-19 06:57:19 +0000

    Update get balance call

--- src/eth/ether.js
+++ src/eth/ether.js
@@ -5,6 +5,7 @@ exports.getBalance = async address => {
   //api key: NVDGQFJ7FXPYK1MQ6D586F6FZ6JRFM2ZGI
   const url = `https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=YourApiKeyToken`
   const response = await axios.get(url);
+  console.log(address + ': '+response.data.result);	
   return Promise.resolve(response.data.result)
 }
 
